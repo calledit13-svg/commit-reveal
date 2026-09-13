b978594c80085fc02829eb655d09e672e517c3ea
calledit13-svg <calledit13@gmail.com>
2026-09-12 22:13:54 -0400

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { hashCommitment, verifyCommitment, buildCommitmentPreimage } from "./hash";
+export type { CommitmentFields } from "./hash";
+export { writeCommitmentMemo, readCommitmentMemo, MEMO_PROGRAM_ID } from "./memo";
+export { verifyRevealedCall } from "./verify";
+export type { VerificationResult } from "./verify";
